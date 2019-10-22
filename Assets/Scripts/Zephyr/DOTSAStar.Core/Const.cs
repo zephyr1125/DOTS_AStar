2b35d3e330d988021ff55c0665e3e34652d8046b
Zephyr Zhang <zephyr1125@gmail.com>
2019-10-22 21:05:24 +0800

    放入Package

--- Assets/Scripts/Zephyr/DOTSAStar.Core/Const.cs
+++
@@ -1,9 +0,0 @@
-namespace Zephyr.DOTSAStar.Core
-{
-    public class Const
-    {
-        public const int MapWidth = 20;
-        public const int MapHeight = 10;
-        public static readonly int[] TerrainCosts = {1,int.MaxValue,3};
-    }
-}
